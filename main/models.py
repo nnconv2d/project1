15e57983e787805c582336adef5dd8fe05d0b041
nnconv2d <wwwept@naver.com>
2024-07-16 20:03:46 +0900

    add: add photo

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     postname = models.CharField(max_length=100)
+    mainphoto = models.ImageField(blank=True, null=True)
     contents = models.TextField()
 
     def __str__(self):
