3eb373d72574f273835980b9eade10bd4f657380
nnconv2d <wwwept@naver.com>
2024-07-25 00:14:38 +0900

    add: add remove function

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib import admin
-from main.views import index, blog, posting, new_post
+from main.views import index, blog, posting, new_post, remove_post
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -13,6 +13,7 @@ urlpatterns = [
     path('blog/', blog),
     path('blog/<int:pk>', posting, name="posting"),
     path('blog/new_post/', new_post),
+    path('blog/<int:pk>/remove/', remove_post),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
