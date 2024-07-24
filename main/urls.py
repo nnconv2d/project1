a271fe15a4d11da82b1e34ff9f38b4f0111de05a
nnconv2d <wwwept@naver.com>
2024-07-25 00:09:45 +0900

    add: add write function

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib import admin
-from main.views import index, blog, posting
+from main.views import index, blog, posting, new_post
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -12,6 +12,7 @@ urlpatterns = [
     # URL/blog에 접속하면 blog 페이지 + URL 이름은 blog
     path('blog/', blog),
     path('blog/<int:pk>', posting, name="posting"),
+    path('blog/new_post/', new_post),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
